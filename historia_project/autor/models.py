c35974b83b9c29ec68682d78db88776841600e58
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2020-03-23 21:54:07 -0300

    Parte do autor esta finalizada

--- historia_project/autor/models.py
+++ historia_project/autor/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
 class Autor(AbstractUser):
-    descricao = models.TextField()
+    descricao = models.TextField(
+        default="Deveria colocar uma descrição sua aqui")
 
     def __str__(self):
         return self.username
+
+    @classmethod
+    def create_novo_autor(self, data):
+        if "descricao" in data:
+            autor = Autor(username=data["username"], descricao=data["descricao"])
+            autor.set_password(data["password"])
+            autor.save()
+            return autor
+        else:
+            autor = Autor(username=data["username"])
+            autor.set_password(data["password"])
+            autor.save()
+            return autor
 
     class Meta:
         verbose_name = "Autor"
