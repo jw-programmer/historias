b7602ac7633bfe0f4b6b40a096ed9711cdd3406a
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2020-03-27 09:50:06 -0300

    Meta de historia criado

--- historia_project/historia/admin.py
+++ historia_project/historia/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Historia
 # Register your models here.
+admin.site.register(Historia)
--- historia_project/historia/models.py
+++ historia_project/historia/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from categoria.models import Categoria
+from genero.models import Genero
 
-# Create your models here.
+
+class Historia(models.Model):
+
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField()
+    data_publicacao = models.DateField(auto_now=True)
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.PROTECT)
+    categorias = models.ManyToManyField(Categoria)
+    genero = models.ManyToManyField(Genero)
+
+    class Meta:
+        verbose_name = ("Historia")
+        verbose_name_plural = ("Historias")
+
+    def __str__(self):
+        return self.titulo
