dada9d511d4757adca057dff447e6108986f3fea
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2020-03-27 09:54:42 -0300

    Meta de capitulo criado

--- historia_project/capitulo/models.py
+++ historia_project/capitulo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from historia.models import Historia
 
-# Create your models here.
+
+class Capitulo(models.Model):
+
+    index = models.IntegerField()
+    titulo = models.CharField(max_length=50)
+    notas = models.TextField(max_length=8000)
+    texto = models.TextField()
+    historia = models.ForeignKey(Historia, on_delete=models.PROTECT)
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'Capitulo'
+        verbose_name_plural = 'Capitulos'
